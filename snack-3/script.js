1f9a52804092dfeac23852c1b65c9d6d8405124e
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-16 15:27:28 +0200

    es3-commit

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,16 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+
+let newArray  = []
+
+for  ( let i=0; i<numbers.length; i++){
+     let number = numbers [i] ;
+
+     
+     newArray.push(number + 1);
+}
+
+console.log(newArray)
+
