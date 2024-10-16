ff609e85404433c5a4a79830e12197d9f72b9057
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-16 17:58:15 +0200

    es-7-commit

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,12 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+
+const studentId2 = students.filter(function(student) {
+  return student.id === 2; 
+});
+
+const result = studentId2[0];
+
+console.log(result); 
