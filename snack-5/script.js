c4686093c5ff0da80e581837220d9d562a3f2c7f
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-16 15:48:20 +0200

    es-5-commit

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,16 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+let newArray = []; 
+
+for (let i = 0; i < nums.length; i++) {
+    let num = nums[i]; 
+
+   
+    if (num % 2 === 0) {
+        newArray.push(num); 
+    }
+}
+
+console.log(newArray); 
